8aae13c7d2d757f080ced8e66138330ff501ea12
Ching June Tao <chingjunetao@1utar.my>
2016-03-11 01:11:13 +0800

    aa
    aa

--- task2.py
+++ task2.py
@@ -17,7 +17,7 @@ ax1.imshow(img)
 ax2.imshow(r, cmap = 'Reds')
 ax3.imshow(g, cmap = 'Greens')
 ax4.imshow(b, cmap = 'Blues')
-plt.savefig('img.png')
+plt.savefig('ori_img.png')
 plt.show()
 
 
@@ -72,8 +72,47 @@ ax2.imshow(Red_30, cmap = 'Reds')
 ax3.imshow(Green_30, cmap = 'Greens')
 ax4.imshow(Blue_30, cmap = 'Blues')
 plt.savefig('new_img.png')
+plt.show()
 
 #save the new image
 img_new2 = Image.fromarray(img_new,'RGB')
 img_new2.save('lowResolution_Tree.jpg')
         
+###### Better resolution 200 ######
+#create new image with better resolution
+Red_S_200 = np.zeros((800,1000))
+Green_S_200 = np.zeros((800,1000))
+Blue_S_200 = np.zeros((800,1000))
+
+for i in range(200):
+    Red_S_200[i][i] = Red_S[i][i]
+    Green_S_200[i][i] = Green_S[i][i]
+    Blue_S_200[i][i] = Blue_S[i][i]
+    
+#combine USV to single matrix
+Red_200 = np.asmatrix(Red_U)* np.asmatrix(Red_S_200)*np.asmatrix(Red_V)
+Green_200 = np.asmatrix(Green_U)* np.asmatrix(Green_S_200)*np.asmatrix(Green_V)
+Blue_200 = np.asmatrix(Blue_U)* np.asmatrix(Blue_S_200)*np.asmatrix(Blue_V)
+
+#combine new RGB into new image
+img_better = img
+img_better[:,:,0] = Red_200
+img_better[:,:,1] = Green_200
+img_better[:,:,2] = Blue_200
+
+#create new plot
+fig = plt.figure(1)
+ax1 = fig.add_subplot(2,2,1)
+ax2 = fig.add_subplot(2,2,2)
+ax3 = fig.add_subplot(2,2,3)
+ax4 = fig.add_subplot(2,2,4)
+ax1.imshow(img_better)
+ax2.imshow(Red_30, cmap = 'Reds')
+ax3.imshow(Green_30, cmap = 'Greens')
+ax4.imshow(Blue_30, cmap = 'Blues')
+plt.savefig('img_better.png')
+plt.show()
+
+#save the new image
+img_better = Image.fromarray(img_better,'RGB')
+img_better.save('BetterResolution_Tree.jpg')
