29adfc1a6d0ac703dab032d3258fc6bd395ad217
Ching June Tao <chingjunetao@1utar.my>
2016-03-11 00:36:03 +0800

    coding
    first commit from git desktop

--- task1.py
+++ task1.py
@@ -1,19 +1,74 @@
 import numpy as np
-#Your optional code here
-#You can import some modules or create additional functions
+import scipy.linalg as sci 
 
 def lu(A, b):
     sol = []
-    # Edit here to implement your code
+    P, L, U = sci.lu(A)  
+    #P is Permutation matrix 
+    #L is Lower Triangular matrix
+    #U is Upper Triangular matrix
+
+    #A = PLU    
+    #Ax = b  =>  PLUx = b
+    sol = np.dot(sci.inv(U),np.dot(sci.inv(L),np.dot(sci.inv(P),b)))
     return list(sol)
 
 def sor(A, b):
     sol = []
-    # Edit here to implement your code
+    A = np.asarray(A)
+    b = np.asarray(b)
+
+    #A = D-L-U 
+    #Compute matrix D     
+    D = np.zeros([len(A),len(A.T)])
+    for i in range(len(A)):
+        D[i][i] = A[i][i]
+        
+    #Compute matrix L
+    L = np.zeros([len(A),len(A.T)])
+    for i in range (len(A)):
+        for j in range (0,i):
+            L[i][j] = -A[i][j]
+    
+    #Compute matrix U      
+    U = np.zeros([len(A),len(A.T)])
+    for i in range (len(A)):
+       for j in range(i+1, len(A)):
+            U[i][j] = -A[i][j]
+            
+    #Calculate matrix Kj
+    Kj = np.dot(sci.inv(D),(L+U))
+    SR = max(sci.eigvals(Kj))     # Spectral Radius
+    w = 2*(1-np.sqrt(1-(SR**2)))/(SR**2)  # Calculate optimal omega
+    
+    Q = np.zeros([len(A),len(A.T)])
+    for i in range (0, len(A)):
+        for j in range (0, len(A)):
+            Q[i][j] = (1/w)*(D[i][j] - w*L[i][j])    
+            
+    X = b
+    for i in range(20):
+        X = np.dot(sci.inv(Q),np.dot((Q-A),X)) + np.dot(sci.inv(Q),b)
+        print("Iteration %1d: " %(i+1), X)
+        
+    sol = X 
     return list(sol)
 
+
+
 def solve(A, b):
-    condition = True # State and implement your condition here
+    A = np.asarray(A)
+    b = np.asarray(b)
+    #check is the eigen values of matrix positive
+    is_eigenvalue_pos = np.all(np.linalg.eigvals(A) > 0)
+    
+    #check is symmetry
+    is_symmetry = (A.T == A).all()
+    
+    #check is the matrix positive definite
+    check = (is_eigenvalue_pos and is_symmetry)    
+    
+    condition = not(check) # State and implement your condition here
     if condition:
         print('Solve by lu(A,b)')
         return lu(A,b)
--- task2.py
+++ task2.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
+from PIL import Image
 
-img=mpimg.imread('Lenna.tiff')
+# new picture Tree.tiff
+img = mpimg.imread('tree.tiff')
 [r,g,b] = [img[:,:,i] for i in range(3)]
 
 
@@ -17,3 +19,58 @@ ax3.imshow(g, cmap = 'Greens')
 ax4.imshow(b, cmap = 'Blues')
 plt.show()
 
+#Decompose Red matrix by using SVD
+Red_U, TempRed_S, Red_V = np.linalg.svd(r, full_matrices = True, compute_uv = True)
+
+#Decompose Green matrix by using SVD
+Green_U, TempGreen_S, Green_V = np.linalg.svd(g, full_matrices = True, compute_uv = True)
+
+#Decompose Blue matrix by using SVD
+Blue_U, TempBlue_S, Blue_V = np.linalg.svd(b, full_matrices = True, compute_uv = True)
+
+# convert Sigma from vector to diagonal matrix
+Red_S = np.zeros((800,1000))
+Green_S = np.zeros((800,1000))
+Blue_S = np.zeros((800,1000))
+
+for i in range(800):
+    Red_S[i][i] = TempRed_S[i]
+    Green_S[i][i] = TempGreen_S[i]
+    Blue_S[i][i] = TempBlue_S[i]
+
+#create new image with lower resolution
+Red_S_30 = np.zeros((800,1000))
+Green_S_30 = np.zeros((800,1000))
+Blue_S_30 = np.zeros((800,1000))
+
+for i in range(30):
+    Red_S_30[i][i] = Red_S[i][i]
+    Green_S_30[i][i] = Green_S[i][i]
+    Blue_S_30[i][i] = Blue_S[i][i]
+    
+#combine USV to single matrix
+Red_30 = np.asmatrix(Red_U)* np.asmatrix(Red_S_30)*np.asmatrix(Red_V)
+Green_30 = np.asmatrix(Green_U)* np.asmatrix(Green_S_30)*np.asmatrix(Green_V)
+Blue_30 = np.asmatrix(Blue_U)* np.asmatrix(Blue_S_30)*np.asmatrix(Blue_V)    
+
+#combine new RGB into new image
+img_new = img
+img_new[:,:,0] = Red_30
+img_new[:,:,1] = Green_30
+img_new[:,:,2] = Blue_30
+
+#create new plot
+fig = plt.figure(1)
+ax1 = fig.add_subplot(2,2,1)
+ax2 = fig.add_subplot(2,2,2)
+ax3 = fig.add_subplot(2,2,3)
+ax4 = fig.add_subplot(2,2,4)
+ax1.imshow(img_new)
+ax2.imshow(Red_30, cmap = 'Reds')
+ax3.imshow(Green_30, cmap = 'Greens')
+ax4.imshow(Blue_30, cmap = 'Blues')
+
+#save the new image
+img_new2 = Image.fromarray(img_new,'RGB')
+img_new2.save('lowResolution_Tree.jpg')
+        
